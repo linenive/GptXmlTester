3b59c98eb67c4edf662218a8b2ff6b31db12e253
주예진 <linenive@naver.com>
2024-03-01 20:14:40 +0900

    스테이터스 창 무한 증식 패치

--- guiMain.py
+++ guiMain.py
@@ -9,17 +9,30 @@ def press_start_game():
 
     guiStatus.create_new_window(gameMain.current_status)
 
+    game_start_button.pack_forget()
+
+    status_button.pack()
+
+    game_info_label.config(text="운명의 주사위는 던져졌다..........")
+
+
 # 메인 윈도우 생성
 root = tk.Tk()
 root.title("회사원 로그라이크")
 
 # 레이블 위젯 추가
-label = tk.Label(root, text="당신은 방금 막 입사했습니다... 최종 목표는 대표의 자리입니다. 힘내보세요.")
-label.pack()
+game_info_label = tk.Label(root, text="당신은 방금 막 입사했습니다... 최종 목표는 대표의 자리입니다. 힘내보세요.")
+game_info_label.pack()
 
 # 버튼 위젯 추가
-button = tk.Button(root, text="시작", command=press_start_game)
-button.pack()
+game_start_button = tk.Button(root, text="시작", command=press_start_game)
+game_start_button.pack()
+
+# 메인 윈도우에 스텟 창 버튼 추가
+status_button = tk.Button(
+        root,
+        text="스테이터스",
+        command=lambda: guiStatus.create_new_window(gameMain.current_status))
 
 # 메인 이벤트 루프 실행
 root.mainloop()
